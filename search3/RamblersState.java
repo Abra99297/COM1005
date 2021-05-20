d96fd83359f6a365e52a1a079fd79b06f4213f82
Yabo Ye <982329316@qq.com>
2021-05-20 23:15:53 +0800

    Try to do some State and Search.java

---
+++ search3/RamblersState.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class RamblersState extends SearchState {
+	
+	//
+	private int MyCoordX;
+	private int MyCoordY;
+	private int[][] Mycoords;
+	
+	//my constructor
+	public RamblersState(int[][] Mycoords, int Mylc) {
+		
+		
+	}
+	
+	//the accessor
+	public {
+	}
+	
+	
+	
+	
+}
