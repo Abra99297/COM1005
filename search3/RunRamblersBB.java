5ea44cacbdff4b8afd95cf2ba5ba89bfc50bd5d1
Yabo Ye <982329316@qq.com>
2021-05-22 01:29:12 +0800

    add some new code

--- search3/RunRamblersBB.java
+++ search3/RunRamblersBB.java
@@ -4,9 +4,10 @@ public class RunMapSearch {
 	  
 	  //Make the map
 	  TerrainMap Mymap = new TerrainMap("tmc.pgm");
+	  Coords Mycoord = new Coords();
 	  
 	  //use the search and state
-	  RamblersSearch Mysearcher = new RamblersSearch();
+	  RamblersSearch Mysearcher = new RamblersSearch(Mymap,);
 	  RamblersState MyState = (RamblersState) new RamblersState();
 	  
 	  //print out the search result
