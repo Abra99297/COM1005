e5cfe6aee2cf2567fadb093b7127492c1a6e4f0e
Yabo Ye <982329316@qq.com>
2021-05-22 19:39:40 +0800

    some improvement

--- search3/RamblersState.java
+++ search3/RamblersState.java
@@ -3,58 +3,61 @@ import java.util.*;
 public class RamblersState extends SearchState {
 	
 	//
-	private TerrainMap Mymap;
 	private Coords Mycoords;
+	private String Myway;
 	
 	//my constructor
-	public RamblersState(TerrainMap Map, Coords Coords) {
-		Mymap = Map;
+	public RamblersState(Coords Coords, String Myway1) {
 		Mycoords = Coords;
+		Myway = Myway1;
 	}
-	
-	//the accessor
-	public TerrainMap getThisMap() {
-		return Mymap;
-	}
-	
+
+	//accessor
 	public Coords getCoords(){
 		return Mycoords;
 	}
 	
 	public boolean goalPredicate(Search searcher) {
 		RamblersSearch Mysearcher = (RamblersSearch) searcher;
-	    Coords goalCD = Mysearcher.getFinalCD(); 
-	    return(Mycoords == goalCD);
+	    Coords goalCD = Mysearcher.getGoalCD(); 
+	    int Y = goalCD.gety();
+	    int X = goalCD.getx();
+	    return(Mycoords.gety() == Y && Mycoords.getx() == X);
 	}
 	
 	public ArrayList<SearchState> getSuccessors(Search searcher){
+		//find the map and the information
 		RamblersSearch Mysearcher = (RamblersSearch) searcher;
-		Coords NextCD = Mysearcher.getFinalCD();
+		TerrainMap MyTmap = Mysearcher.getMymap();
+		int[][] MyTnmp = MyTmap.getTmap();
+		int MyWidth = MyTmap.getWidth();
+		int MyDepth = MyTmap.getDepth();
+		int Mylc = 0;
+		
+		//find the position in the map
+		int Y1 = Mycoords.gety();
+		int X1 = Mycoords.getx();
 		
 		//make list for RamblersState and searchState
 		ArrayList<RamblersState> Rslist = new ArrayList<RamblersState>();
 		ArrayList<SearchState> Slist = new ArrayList<SearchState>();
 		
-		//find out the height and the height difference
-		int Myheight1 =Mycoords.gety() - Mycoords.getx();
-		int Myheight2 =NextCD.gety() - NextCD.getx();
-		int Hdiffer = Myheight2 - Myheight1;
-		
 		//find the right coords
-		if(Myheight2 <= Myheight1) {
-			
-		}
+		
 		
 		return Slist;
 	}
 	
 	  public boolean sameState(SearchState RS1) {
 		  RamblersState Mysearcher1 = (RamblersState) RS1;
-		    return (Mycoords == Mysearcher1.getCoords());
+		  Coords MyCoords2 = Mysearcher1.getCoords();
+		  int Y2 = MyCoords2.gety();
+		  int X2 = MyCoords2.getx();
+		  return (Mycoords.gety()==Y1 && Mycoords.getx()==X1);
 		  }
 	
 	
 	  public String toString() {
-		  return "Ramblers State:" +Mycoords;
+		  return "Ramblers State:" + Mycoords.gety() + Mycoords.getx();
 	  }
 }
